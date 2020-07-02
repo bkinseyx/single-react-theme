3e1e59c13207cd9e71b522cf68f62ffe0f5ca935
Ben Kinsey <bkinsey@xtivia.com>
2020-07-02 13:33:44 -0500

    web content with theme react works

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -5,18 +5,29 @@ import { App } from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
-document.querySelectorAll('[data-react-app]').forEach((el) => {
-  const reactApp = el.getAttribute('data-react-app');
-  const reactConfig = JSON.parse(el.getAttribute('data-react-config') ?? '');
-  if (!reactApp) {
-    return;
+declare global {
+  interface Window {
+    initThemeReact: Function;
   }
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App reactApp={reactApp} reactConfig={reactConfig} />
-      </Provider>
-    </React.StrictMode>,
-    el
-  );
-});
+}
+window.initThemeReact = () => {
+  document.querySelectorAll('[data-react-app]').forEach((el) => {
+    if (el.innerHTML) {
+      return;
+    }
+    const reactApp = el.getAttribute('data-react-app');
+    const rawReactConfig = el.getAttribute('data-react-config');
+    const reactConfig = rawReactConfig ? JSON.parse(rawReactConfig) : {};
+    if (!reactApp) {
+      return;
+    }
+    ReactDOM.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App reactApp={reactApp} reactConfig={reactConfig} />
+        </Provider>
+      </React.StrictMode>,
+      el
+    );
+  });
+};
--- src/templates/react/react_js.ftl
+++ src/templates/react/react_js.ftl
@@ -1,3 +1,3 @@
 <script src="/o/single-react-theme/images/react/static/js/runtime-main.ecfefd04.js"></script>
 <script src="/o/single-react-theme/images/react/static/js/6.b7518456.chunk.js"></script>
-<script src="/o/single-react-theme/images/react/static/js/main.166aac0d.chunk.js"></script>
+<script src="/o/single-react-theme/images/react/static/js/main.dfc206ad.chunk.js"></script>
